4d6fcf6fcac9530323154507483d4bd5766812e9
Danielle Ejiogu <111812010+DanChigo@users.noreply.github.com>
2025-04-04 16:13:05 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mysite/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
