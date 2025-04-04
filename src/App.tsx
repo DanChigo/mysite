749b75d9d38aa7f752d1b38b48c3fd0f05bec329
Danielle Ejiogu <111812010+DanChigo@users.noreply.github.com>
2025-04-04 19:31:38 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import  Navbar  from './components/Navbar'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import { Home } from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Blog from './components/Blog'
 import { styleReset } from 'react95'
-import original from 'react95/dist/themes/original';
+import original from 'react95/dist/themes/original'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 /* Original Windows95 font (optional) */
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2'
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2'
 
 const GlobalStyles = createGlobalStyle`
   ${styleReset}
@@ -29,29 +29,29 @@ const GlobalStyles = createGlobalStyle`
   body, input, select, textarea {
     font-family: 'ms_sans_serif';
   }
-`;
+`
 
 function App() {
   return (
     <div>
-    <GlobalStyles />
-    <ThemeProvider theme={original}>
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/blog" element={<Blog />} />
-
-            {/* Add other routes when they're ready */}
-          </Routes>
-        </main>
-      </div>
-    </Router>
-    </ThemeProvider>
+      <GlobalStyles />
+      <ThemeProvider theme={original}>
+        {/* Changed from BrowserRouter to HashRouter and removed basename */}
+        <Router>
+          <div className="min-h-screen bg-gray-50">
+            <Navbar />
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/blog" element={<Blog />} />
+                {/* Add other routes when they're ready */}
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </ThemeProvider>
     </div>
   )
 }
